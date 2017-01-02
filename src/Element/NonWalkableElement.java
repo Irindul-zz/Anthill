4aa97cb98865af262f06f17165a970d5a528939e
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-02 17:16:06 +0100

    Adding NonWalkableElement and WalkableElement

---
+++ src/Element/NonWalkableElement.java
@@ -0,0 +1,12 @@
+package Element;
+
+/**
+ * Created by Irindul on 02/01/2017.
+ */
+public class NonWalkableElement extends AbstractElement {
+
+    @Override
+    public boolean isWalkable() {
+        return false;
+    }
+}
--- src/Mapping/Map.java
+++ src/Mapping/Map.java
@@ -1,9 +1,9 @@
 package Mapping;
 
-import Element.Element;
+import Element.AbstractElement;
 /**
  * Created by Irindul on 25/12/2016.
  */
 public class Map {
-    Element[][] element;
+    AbstractElement[][] elements;
 }
