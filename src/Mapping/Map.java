ceefab2757683f1cf8174ea6cc4d911cef74dfc3
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-05 13:25:14 +0100

    Refactored Map

--- src/Mapping/Map.java
+++ src/Mapping/Map.java
@@ -1,9 +1,21 @@
 package Mapping;
 
+import Anthill.Anthill;
+import Collections.FoodSupplyCol;
+import Collections.PheromoneCol;
 import Element.AbstractElement;
 /**
  * Created by Irindul on 25/12/2016.
  */
 public class Map {
-    AbstractElement[][] elements;
+
+    private FoodSupplyCol foodSupplies;
+    private PheromoneCol pheromones;
+    private Anthill anthill;
+
+    /* Might be some things to add here*/
+
+    public void generateMap(){
+
+    }
 }
