a85d5a52d568236e180ad03cfc743ef0630d911e
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-05 13:17:09 +0100

    Added PheromoneCol

---
+++ src/Collections/PheromoneCol.java
@@ -0,0 +1,42 @@
+package Collections;
+
+import Anthill.Pheromone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Irindul on 05/01/2017.
+ */
+public class PheromoneCol {
+
+    private List<Pheromone> pheromones;
+
+    public PheromoneCol(){
+        pheromones = new ArrayList<>();
+    }
+
+
+    public Pheromone get(int i){
+
+        return null;
+    }
+
+    public void add(Pheromone p, int i){
+
+    }
+
+    public void add(Pheromone p){
+
+    }
+
+    public void remove(int i){
+
+    }
+
+    public int size(){
+
+        return 0;
+    }
+
+}
