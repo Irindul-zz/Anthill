89e25d0b2a38adedae0d1fc712e20193d451c2c6
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-05 19:43:28 +0100

    Created Mind that implements Memory

---
+++ src/Ant/Mind.java
@@ -0,0 +1,39 @@
+package Ant;
+
+import Brain.Memory;
+import Mapping.Direction;
+
+/**
+ * Created by Irindul on 05/01/2017.
+ */
+public class Mind implements Memory {
+
+
+    //Simple implementation of a Stack with linked-list.
+    private Node first = null;
+
+    private static class Node {
+        private Direction dir;
+        private Node next;
+
+        public Node(Direction dir) {
+            this.dir = dir;
+        }
+    }
+
+    @Override
+    public void keepTrack(Direction d) {
+        Node newFirst = new Node(d);
+        newFirst.next = first;
+        first = newFirst;
+
+    }
+
+    @Override
+    public Direction rollBack() {
+        Node oldFirst = first;
+        first = first.next;
+
+        return oldFirst.dir;
+    }
+}
