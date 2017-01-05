3812f4406d12adf0ca36037cc3c89937a5264d48
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-05 16:16:25 +0100

    Proba method

--- src/Ant/Proba.java
+++ src/Ant/Proba.java
@@ -4,4 +4,28 @@ package Ant;
  * Created by Irindul on 05/01/2017.
  */
 public class Proba {
+
+    int probas[];
+    int cumFreq[];
+
+    public Proba() {
+
+        probas = new int[8];
+        cumFreq = new int[8];
+
+    }
+
+    public void initialize(){
+        for(int a: probas){
+            a = 0;
+        }
+    }
+
+    public void calulateFrequencies(){
+
+        cumFreq[0] = probas[0]*100; //Probas will be converted in %
+        for (int i = 1; i < 8;i++) {
+            cumFreq[i] = cumFreq[i-1] + probas[i]*100;
+        }
+    }
 }
