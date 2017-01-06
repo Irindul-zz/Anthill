1f81a31ff5c6a9174ce764de097f5d09e8d5aee7
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-06 12:53:22 +0100

    Adding comments to Mind

--- src/Ant/Mind.java
+++ src/Ant/Mind.java
@@ -12,6 +12,8 @@ public class Mind implements Memory {
     //Simple implementation of a Stack with linked-list.
     private Node first = null;
 
+
+    //The node is static so that it doesn't take much memory access and assure that it cannot use Mind attributes.
     private static class Node {
         private Direction dir;
         private Node next;
@@ -23,17 +25,18 @@ public class Mind implements Memory {
 
     @Override
     public void keepTrack(Direction d) {
-        Node newFirst = new Node(d);
-        newFirst.next = first;
-        first = newFirst;
+        Node newFirst = new Node(d); //We create a new Node
+        newFirst.next = first; //We tell the new node that the node next to it is first.
+        first = newFirst; //The new first is now the new node created.
 
     }
 
     @Override
     public Direction rollBack() {
-        Node oldFirst = first;
-        first = first.next;
+        Node oldFirst = first; //We save the current first element.
+        first = first.next; // We say that the new first is the element right after.
 
+        //We return a Direction reversed so that we can go back.
         return Direction.reverse(oldFirst.dir);
     }
 }
