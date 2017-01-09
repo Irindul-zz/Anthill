a6c7ea9456923a6437d51db535d00bd1fe444f8e
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-09 21:58:16 +0100

    Added BasicSensor class that implements Sense

---
+++ src/main/Ant/BasicSensor.java
@@ -0,0 +1,19 @@
+package main.Ant;
+
+import main.Mapping.Map;
+import main.Mapping.Position;
+
+/**
+ * Created by Irindul on 09/01/2017.
+ */
+public class BasicSensor implements Sense {
+    @Override
+    public void detectFood(Position pos, Map map) {
+
+    }
+
+    @Override
+    public void detectPheromones(Position pos, Map map) {
+
+    }
+}
