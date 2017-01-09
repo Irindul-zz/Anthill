88d732c4585f8d03e07c2e3509e1c8af7d4bc1ff
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-09 21:58:36 +0100

    Added Sensor attribute in Ant

--- src/main/Ant/Ant.java
+++ src/main/Ant/Ant.java
@@ -9,9 +9,11 @@ public class Ant{
 
     protected boolean hasFood;
     protected Direction direction;
+    protected Sense sensor;
 
     public Ant(Direction direction) {
         this.direction = direction;
+        sensor = new BasicSensor(); //this line must be overwritten for different sensors.
     }
 
     public Ant(){
