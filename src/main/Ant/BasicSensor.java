0823491ed0fe8c88e5c4f376418e050812936265
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-09 22:01:19 +0100

    Comment BasicSensor

--- src/main/Ant/BasicSensor.java
+++ src/main/Ant/BasicSensor.java
@@ -7,6 +7,8 @@ import main.Mapping.Position;
  * Created by Irindul on 09/01/2017.
  */
 public class BasicSensor implements Sense {
+
+    //Should be an attibuts containing the surrounding cases. ( Will be added TODO create class containing results and add it)
     @Override
     public void detectFood(Position pos, Map map) {
 
