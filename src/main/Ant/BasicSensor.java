8ccc255c5d23aa47b93d11bbf0159c33100572c0
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-10 13:42:30 +0100

    Detect pheromones

--- src/main/Ant/BasicSensor.java
+++ src/main/Ant/BasicSensor.java
@@ -1,5 +1,6 @@
 package main.Ant;
 
+import main.Mapping.Direction;
 import main.Mapping.Map;
 import main.Mapping.Position;
 
@@ -8,7 +9,12 @@ import main.Mapping.Position;
  */
 public class BasicSensor implements Sense {
 
-    //Should be an attibuts containing the surrounding cases. ( Will be added TODO create class containing results and add it)
+    double[] results;
+
+    public BasicSensor() {
+        results = new double[9];
+    }
+
     @Override
     public void detectFood(Position pos, Map map) {
 
@@ -16,6 +22,32 @@ public class BasicSensor implements Sense {
 
     @Override
     public void detectPheromones(Position pos, Map map) {
+        int x = pos.getX();
+        int y = pos.getY();
 
+        Position north = new Position(x+1, y);
+        Position northeast = new Position(x+1, y+1);
+        Position east = new Position(x, y+1);
+        Position southeast = new Position(x-1, y+1);
+        Position south = new Position(x-1, y);
+        Position southwest = new Position(x-1, y-1);
+        Position west = new Position(x, y-1);
+        Position northwest = new Position(x+1, y-1);
+
+        results[Direction.NORTH.ordinal()] = map.getPheromoneQuantityAt(north);
+        results[Direction.NORTHEAST.ordinal()] = map.getPheromoneQuantityAt(northeast);
+        results[Direction.EAST.ordinal()] = map.getPheromoneQuantityAt(east);
+        results[Direction.SOUTHEAST.ordinal()] = map.getPheromoneQuantityAt(southeast);
+        results[Direction.SOUTH.ordinal()] = map.getPheromoneQuantityAt(south);
+        results[Direction.SOUTHWEST.ordinal()] = map.getPheromoneQuantityAt(southwest);
+        results[Direction.WEST.ordinal()] = map.getPheromoneQuantityAt(west);
+        results[Direction.NORTHWEST.ordinal()] = map.getPheromoneQuantityAt(northwest);
+
+
+    }
+
+    @Override
+    public double getResults(int i) {
+        return results[i];
     }
 }
