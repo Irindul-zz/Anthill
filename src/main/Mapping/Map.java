3b245bedab6c14885119971298dc76489e09cbf0
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-12 08:31:12 +0100

    Added Cells

--- src/main/Mapping/Map.java
+++ src/main/Mapping/Map.java
@@ -3,6 +3,7 @@ package main.Mapping;
 import main.Anthill.Anthill;
 import main.Collections.FoodSupplyCol;
 import main.Collections.PheromoneCol;
+import main.Element.Cell;
 
 /**
  * Created by Irindul on 25/12/2016.
@@ -12,6 +13,7 @@ public class Map {
     private FoodSupplyCol foodSupplies;
     private PheromoneCol pheromones;
     private Anthill anthill;
+    private Cell[][] cells;
 
     /* Might be some things to add here*/
     /* Don't know what constructors to add*/
