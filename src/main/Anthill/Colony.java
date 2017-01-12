4210be0d48ba9fb87c90719af9741f53dec0c052
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-12 11:39:40 +0100

    Merge resolve

--- src/main/Anthill/Colony.java
+++ src/main/Anthill/Colony.java
@@ -5,21 +5,15 @@ import main.Collections.FoodSupplyCol;
 import main.Collections.PheromoneCol;
 import main.Element.FoodSupply;
 import main.Mapping.Map;
-<<<<<<< HEAD
-=======
 import main.Mapping.Position;
->>>>>>> 246b5b64dd973449674bfb16bf5f3fe672031123
-
 /**
  * Created by Irindul on 25/12/2016.
  */
 public class Colony {
-<<<<<<< HEAD
-=======
+
     private FoodSupplyCol foodSupplies;
     private PheromoneCol pheromones;
     private Anthill anthill;
->>>>>>> 246b5b64dd973449674bfb16bf5f3fe672031123
     private Map map;
 
     public void addFoodSupply(FoodSupply fs){
@@ -34,15 +28,12 @@ public class Colony {
 
     }
 
-<<<<<<< HEAD
-
-=======
     public int getPheromoneQuantityAt(Position p){
         return 0;
     }
 
-    public  Boolean detectFood(){
+    public boolean detectFood(){
         return true;
     }
->>>>>>> 246b5b64dd973449674bfb16bf5f3fe672031123
+
 }
