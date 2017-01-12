aed4c93a409346c81d8af5cd542d85c5d3b74dd1
Kristoffer Clasen <kristoffer.brastrup@etu.univ-lyon1.fr>
2017-01-12 09:37:05 +0100

    First push

--- src/main/Anthill/Anthill.java
+++ src/main/Anthill/Anthill.java
@@ -1,7 +1,19 @@
 package main.Anthill;
 
+import main.Ant.Ant;
+
+import java.util.List;
+
 /**
  * Created by Irindul on 25/12/2016.
  */
 public class Anthill{
+    List<Ant> ants;
+
+
+    public List getAnts()
+    {
+        return this.ants;
+    }
+
 }
