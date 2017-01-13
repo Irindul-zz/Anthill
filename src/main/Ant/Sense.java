ffaa77426d0c2e332b622403a905e73e577a93dd
Eriwyr <chabrier.valentin@gmail.com>
2017-01-13 14:40:29 +0100

    fix error in sense

--- src/main/Ant/Sense.java
+++ src/main/Ant/Sense.java
@@ -10,13 +10,9 @@ import main.Mapping.Position;
  */
 public interface Sense {
 
-<<<<<<< HEAD
     boolean detectFood(Position pos, Map map);
-    void detectPheromones(Position pos, Map map);
-=======
-    void detectFood(Position pos, Map map);
     void detectPheromones(Position pos, PheromoneCol p);
->>>>>>> 246b5b64dd973449674bfb16bf5f3fe672031123
+
     double getResults(int i);
     //maybe detectObstacles(Position pos, Map map);
 
