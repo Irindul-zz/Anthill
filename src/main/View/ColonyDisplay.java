3c71f485fd9c9a61de33826ed2af9da93b1d95ec
KristerBC <kristoffer.brastrup@etu.univ-lyon1.fr>
2017-01-27 15:40:34 +0100

    added buttons and css

--- src/main/View/ColonyDisplay.java
+++ src/main/View/ColonyDisplay.java
@@ -5,8 +5,20 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import main.Anthill.Colony;
@@ -37,23 +49,74 @@ public class ColonyDisplay extends Application{
         stage.setHeight(750+38);
         stage.setTitle("Anthill");
 
-        Group root = new Group();
-        Scene scene = new Scene(root);
+        Group group_menu = new Group();
+        Scene scene_menu = new Scene(group_menu);
+        scene_menu.setFill(Color.AZURE);
+        scene_menu.getStylesheets().add("style.css");
+
+        Group group_sim = new Group();
+        Scene scene_sim = new Scene(group_sim);
+
 
 
         MapDisplay mapD = new MapDisplay(map);
-        root.getChildren().add(mapD);
+        group_sim.getChildren().add(mapD);
         for (AntDisplay antD: antsDisplay) {
-            root.getChildren().add(antD);
+            group_sim.getChildren().add(antD);
         }
-        System.out.println("hej");
+
+        Text text = new Text("Please choose the intelligens for the ants");
+
+
+        Label label_chooseAnts = new Label(text.getText());
+            text.setId("menuTitle");
+            text.applyCss();
+            text.setLayoutY(40);
+            text.setLayoutX(90);
+        group_menu.getChildren().add(text);
+
+        VBox vbButtons = new VBox();
+            vbButtons.setSpacing(80);
+            vbButtons.setLayoutY(text.getLayoutBounds().getHeight()+200);
+            vbButtons.setPadding(new Insets(0, 20, 10, 20));
+            vbButtons.setPrefWidth(stage.getWidth()-16);
+            vbButtons.setAlignment(Pos.CENTER);
+
+            Button button_basicAnts = new Button("Basic Ants");
+            button_basicAnts.setId("dark-blue");
+            button_basicAnts.applyCss();
+            button_basicAnts.setMaxWidth(Double.MAX_VALUE);
+            button_basicAnts.setPrefHeight(100);
+
+            Button button_brainyAnts = new Button("Brainy Ants");
+            button_brainyAnts.setId("dark-blue");
+            button_brainyAnts.applyCss();
+            button_brainyAnts.setMaxWidth(Double.MAX_VALUE);
+            button_brainyAnts.setPrefHeight(100);
+
+            vbButtons.getChildren().addAll(button_basicAnts,button_brainyAnts);
+        group_menu.getChildren().add(vbButtons);
+
+        button_basicAnts.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                ///////SET LEVEL OF ANTS
+                stage.setScene(scene_sim);
+            }
+        });
+
+        button_brainyAnts.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                stage.setScene(scene_sim);
+            }
+        });
+
         //ColonyDisplay.antsDisplay[i].setPosition(ant.getPosition());
         //ant.setOnKeyPressed(keyEvent -> ant.setPosition(p2));
 
         fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
         fiveSecondsWonder.play();
 
-        stage.setScene(scene);
+        stage.setScene(scene_menu);
         stage.show();
 
 
