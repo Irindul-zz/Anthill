4151f89a2e467a9e1a0dd93afa431fc02f199b4b
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-27 18:56:03 +0100

    Continued Dijkstra

--- src/main/Graph/Dijkstra.java
+++ src/main/Graph/Dijkstra.java
@@ -2,9 +2,7 @@ package main.Graph;
 
 import main.Mapping.Position;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Irindul on 27/01/2017.
@@ -22,18 +20,34 @@ public class Dijkstra {
         Node nodeStart = graph.nodeFromPosition(start);
         Node nodeGoal = graph.nodeFromPosition(goal);
         frontier.add(nodeStart);
-        Set<Edge> cameFrom = new HashSet<>();
-
+        HashMap<Node, Node> cameFrom = new HashMap<>();
+        cameFrom.put(nodeStart, null);
         while(! frontier.isEmpty()){
             Node current = frontier.removeFirst();
 
             if(current == nodeGoal)
                 break;
 
-            for(Node next: graph.neighbours(current)){
-
+            for(Node next: graph.neighbours(current)){ // TODO: 27/01/2017 change with real Dijkstra
+                if( ! cameFrom.containsKey(next)){
+                    frontier.add(next);
+                    cameFrom.put(next, current);
+                }
             }
         }
+
+
+        Node current = nodeGoal;
+        List<Node> path = new ArrayList<>();
+
+        path.add(current);
+        while (current != nodeStart){
+            current = cameFrom.get(current);
+            path.add(current);
+        }
+        path.add(nodeStart);
+
+        //TODO convert Node path into direction path => rollBack
 
 
 
